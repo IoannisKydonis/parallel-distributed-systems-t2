804d33278538df7be393d808e28f63caf778e0c4
Stavrenas <stavrina25@gmail.com>
2021-01-10 20:42:28 +0200

    Refactored code

--- v1.c
+++ v1.c
@@ -5,6 +5,7 @@
 #include "utilities.h"
 #include <mpi.h>
 
+struct knnresult smallKNN(double *x, double *y, int n, int m, int d, int k, int indexOffset);
 
 int main(int argc, char *argv[]) {
     
@@ -31,9 +32,9 @@ int main(int argc, char *argv[]) {
    k=3;
    n=15;
 
-   int SelfTID, NumTasks, t;
+   int SelfTID, NumTasks;
    MPI_Status mpistat;
-   MPI_Request mpireq;
+   MPI_Request mpireq;  //initialize MPI environment
    MPI_Init( &argc, &argv );
    MPI_Comm_size( MPI_COMM_WORLD, &NumTasks );
    MPI_Comm_rank( MPI_COMM_WORLD, &SelfTID );
@@ -53,32 +54,30 @@ int main(int argc, char *argv[]) {
    MPI_Isend(X,sentElements,MPI_DOUBLE,findDestination(SelfTID, NumTasks),55,MPI_COMM_WORLD, &mpireq);  //send self block
 
    int sender=findSender(SelfTID, NumTasks);
-   int receivedArrayIndex;
-   int receivedElements;
-   struct knnresult newResult;
-   int off;
-   struct knnresult mergedResult;
+   int receivedArrayIndex,receivedElements; //initialize variables for use in loop
+   int loopOffset;
 
-   struct knnresult result,previousResult;
+   struct knnresult result,previousResult,newResult,mergedResult;
    result=smallKNN(X,X,points,points,d,k,offset);
    previousResult=result;
    mergedResult=result;
 
-   for (int i=1; i<NumTasks; i++){  //loop
+   for (int i=1; i<NumTasks; i++){  //ring communication
 
    receivedArrayIndex=findBlockArrayIndex(SelfTID, i, NumTasks);
    receivedElements=totalPoints[receivedArrayIndex] * d;
    double * Y= malloc(receivedElements * sizeof(double));
+
    MPI_Recv(Y,receivedElements,MPI_DOUBLE,sender,55,MPI_COMM_WORLD,&mpistat); //receive from previous process
    MPI_Isend(Y,receivedElements,MPI_DOUBLE,findDestination(SelfTID, NumTasks),55,MPI_COMM_WORLD, &mpireq); //send the received array to next process
 
-   off=findIndexOffset(SelfTID,i,NumTasks,totalPoints);
-   newResult=smallKNN(X,Y,points,totalPoints[receivedArrayIndex],d,k,off);
+   loopOffset=findIndexOffset(SelfTID,i,NumTasks,totalPoints);
+   newResult=smallKNN(X,Y,points,totalPoints[receivedArrayIndex],d,k,loopOffset);
    mergedResult=updateKNN(newResult,previousResult);
    previousResult=mergedResult;
    }
 
-    if (SelfTID == 0) {
+    if (SelfTID == 0) {    //send every result to the first process for printing
         printResult(mergedResult);
         for (int i = 1; i < NumTasks; i++) {
             char *deserialized = malloc(n * k * 27 + n + 1);
@@ -94,3 +93,78 @@ int main(int argc, char *argv[]) {
    return(0);
 }
 
+
+
+struct knnresult smallKNN(double *x, double *y, int n, int m, int d, int k, int indexOffset)
+{ 
+    struct knnresult *result = malloc(sizeof(struct knnresult));
+
+    double *xx = malloc(n * d * sizeof(double));
+    hadamardProduct(x, x, xx, n * d);
+
+    double *yy = malloc(m * d * sizeof(double));
+    hadamardProduct(y, y, yy, m * d);
+
+    double *xxSum = malloc(n * sizeof(double));
+    for (int i = 0; i < n; i++)
+    {
+        xxSum[i] = cblas_dasum(d, xx + i * d, 1);
+    }
+
+    double *yySum = malloc(m * sizeof(double));
+    for (int i = 0; i < m; i++)
+    {
+        yySum[i] = cblas_dasum(d, yy + i * d, 1);
+    }
+
+    double *xy = malloc(n * m * sizeof(double));
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, n, m, d, -2, x, d, y, d, 0, xy, m);
+
+    double *dist = malloc(n * m * sizeof(double));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            double distanceSquared = xxSum[i] + xy[i * m + j] + yySum[j];
+            if (distanceSquared <= 0)
+                dist[i * m + j] = 0;
+            else
+                dist[i * m + j] = sqrt(distanceSquared);
+        }
+    }
+
+    int *indexValues = (int *)malloc(n * m * sizeof(int));
+    for (int i = 0; i < n * m; i++)
+        indexValues[i] = i+indexOffset;
+
+    int kMin = k > n ? n : k;
+    kMin = kMin > m ? m : kMin;
+
+    int * indexesRowMajor=(int*)malloc(n*k*sizeof(int));
+    double * distRowMajor=(double*)malloc(n*k*sizeof(double));
+    for (int i = 0; i < n * k; i++) {
+        distRowMajor[i] = INFINITY;
+        indexesRowMajor[i] = -1;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < kMin; j++)
+        {
+            int idx;
+            distRowMajor[i*k +j] = kNearest(dist, indexValues, i * m, (i + 1) * m - 1, j + 1, &idx);
+            indexesRowMajor[i*k +j] = idx - i * m;
+        }
+    }
+
+    result->ndist=distRowMajor;
+    result->nidx=indexesRowMajor;
+    result->m=n;
+    result->k=k;
+
+    free(xx);
+    free(yy);
+    free(xy);
+    free(xxSum);
+    free(yySum);
+    return *result;
+}
